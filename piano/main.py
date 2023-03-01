529e415a6bda4c6e520d1832373595e978839586
Filip Plass <filipplass@gmail.com>
2023-03-01 18:46:25 +0100

    octaves fixed

---
+++ piano/main.py
@@ -0,0 +1,30 @@
+import play
+import pygame
+
+play.set_backdrop('light blue')
+sounds = []
+keys = []
+octaves = ["", "f", "g"]
+for i in range(3):
+
+    for j in range(8):
+        newKey = play.new_box(color="grey", width=20, height=80, x = -295 + j*25 + i*40 +i*160)
+        keys.append(newKey)
+        sounds.append(pygame.mixer.Sound(octaves[i] + str(j+1)+'.ogg'))
+    
+
+@play.when_program_starts
+def start():
+    pygame.mixer_music.load('./hello.mp3')
+    pygame.mixer_music.play()
+
+@play.repeat_forever
+async def play_piano():
+    for i in range(len(keys)):
+        if keys[i].is_clicked:
+            keys[i].color = "white"
+            sounds[i].play()
+            await play.timer(seconds=0.3)
+            keys[i].color = "grey"
+
+play.start_program()
