6e18e53ecf8671e54757ce76f24bd8e8189c4e73
Filip Plass <filipplass@gmail.com>
2023-03-29 18:19:29 +0200

    moving fix

--- eggs/main.py
+++ eggs/main.py
@@ -10,20 +10,20 @@ redline = play.new_box(color="red", width=800, height=10, y= -300)
 basket = play.new_image(image="./basket.png", size=200, y=-150)
 egg = play.new_circle(color="white", radius=20)
 fail = play.new_text(color='red', font_size=50, words="You failed!")
-scoreText = play.new_text(font_size=38, x=100, y=100)
+scoreText = play.new_text(font_size=38, x=300, y=270)
 
 async def spawnEgg():
     randomPlace = random.randint(1,4)
     
-    await play.timer(seconds=2)
+    
     if randomPlace == 1:
         egg.go_to(x=-290, y =225)
     if randomPlace == 2:
         egg.go_to(x=290, y =125)
     if randomPlace == 3:
-        egg.go_to(x=-290, y =225)
+        egg.go_to(x=-290, y =125)
     if randomPlace == 4:
-        egg.go_to(x=290, y =125)
+        egg.go_to(x=290, y =225)
 
 
 
@@ -35,14 +35,15 @@ def start():
     platform2.start_physics(can_move=False)
     platform3.start_physics(can_move=False)
     platform4.start_physics(can_move=False)
-    egg.start_physics(can_move=True, stable=False)
+    egg.start_physics(can_move=True, stable=False, bounciness=0)
     redline.start_physics(can_move=False)
+    basket.start_physics(can_move=True, stable=True)
 
 @play.repeat_forever
 async def do():
     global score
     scoreText.words = str(score)
-    await spawnEgg()
+    
     if(play.key_is_pressed('w')):
         basket.y+=10
     if(play.key_is_pressed('s')):
@@ -62,6 +63,8 @@ async def do():
         platform4.hide()
         egg.hide()
         basket.hide()
+    else:
+        await spawnEgg()
     
 
 play.start_program()
